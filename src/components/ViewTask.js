b1de7a7d501d39988753e11f763a452d4f74fe4d
Patil, Sachin G <sachin.g.patil@accenture.com>
2020-11-12 16:59:32 +0530

    class name updated

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -16,17 +16,17 @@ const Task = () => {
     setTask(res.data);
   };
   return (
-    <div class="d-flex justify-content-center">
+    <div className="d-flex justify-content-center">
     <div className="card col-md-8 col-sm-12 p-5">
       <Link className="btn btn-outline-primary col-md-3 col-sm-6 pull-right mb-4" to="/tasks/add">
         back to Home
       </Link>
       <h1 className="display-5">Task Id: {id}</h1>
-        <div class="text-center w-100 p-4">
-            <div class="card-body">
-                <h5 class="card-title">{task.title}</h5>
-                <p class="card-text">Tast description goes here...</p>
-                <button class="btn btn-primary">{task.completed}</button>
+        <div className="text-center w-100 p-4">
+            <div className="card-body">
+                <h5 className="card-title">{task.title}</h5>
+                <p className="card-text">Tast description goes here...</p>
+                <button className="btn btn-primary">{task.completed}</button>
             </div>
             
         </div>
